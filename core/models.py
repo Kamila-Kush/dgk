aa1d0e2d61d62e3b0488bd29eb682da33395c751
Kamila-Kush <kush.kamila@gmail.com>
2023-08-28 18:19:28 +0900

    model Management

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Management(models.Model):
+    name = models.CharField(max_length=100, verbose_name='ФИО')
+    description = models.TextField()
+    photo = models.ImageField( null=True, blank=True)
+
+
