8dd5f5ca169f662b174eccdf9642e0b9475a521a
Kamila-Kush <kush.kamila@gmail.com>
2023-08-30 13:40:30 +0900

    added views for tab About Us

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', homepage, name='homepage')
+    path('', homepage, name='homepage'),
+
+    path('about-us/', about_us, name="about_us"),
+    path('structure/', structure_of_company, name="structure"),
+    path('culture/', corporative_culture, name="culture"),
+    path('management/', ManagementList.as_view(), name="management"),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView
+from .models import *
 
 def homepage(request):
     if request.method == "POST":
         return HttpResponse("Метод не разрешён, только GET", status=405)
     return render(request=request, template_name="index.html")
+
+def about_us(request):
+    return render(request=request, template_name="about_us.html")
+
+def structure_of_company(request):
+    return render(request=request, template_name="structure_of_company.html")
+
+def corporative_culture(request):
+    return render(request=request, template_name="culture.html")
+
+class ManagementList(ListView):
+    template_name = "management.html"
+    model = Management
+
